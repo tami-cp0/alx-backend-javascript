05d42631861982b9302873bd78c30241ef5c0ad4
tami-cp0 <findtamilore>
2024-05-27 12:46:06 +0100

    task 8 done

--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -3,9 +3,8 @@ function getCurrentYear() {
   return date.getFullYear();
 }
 
-export default function getBudgetForCurrentYear(income, gdp, capita) {
-
-  [`income-${getCurrentYear()}`]: `${income}`
-  // [`gdp-${getCurrentYear()}`]: gdp,
-  // [`capita-${getCurrentYear()}`]: capita,
-}
+export default (income, gdp, capita) => ({
+  [`income-${getCurrentYear()}`]: income,
+  [`gdp-${getCurrentYear()}`]: gdp,
+  [`capita-${getCurrentYear()}`]: capita,
+});
