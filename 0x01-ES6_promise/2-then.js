7697f467d858283bd30cad458c72d2f66bcf3a81
tami-cp0 <findtamilore>
2024-05-29 02:16:04 +0100

    task 2 done

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,11 @@
+export default function handleResponseFromAPI(promise) {
+promise.then(() => {
+  return { status: 200, body: 'Success' }
+})
+.catch(() => {
+  throw new Error();
+})
+.finally(() => {
+  console.log('Got a response from the API');
+});
+}
