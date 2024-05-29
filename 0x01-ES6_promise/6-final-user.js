90f40a52c4d4bf6305c5a4db66ad734d5b223336
tami-cp0 <findtamilore>
2024-05-29 16:53:20 +0100

    syntax change

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,13 +1,11 @@
-import signUpUser from '4-user-promise.js';
-import uploadPhoto from '5-photo-reject.js';
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise
     .all([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((responses) => {
-      return responses.map((response) => ({
-        status: response.status,
-        value: response.message,
-      }));
-    });
+    .then((responses) => responses.map((response) => ({
+      status: response.status,
+      value: response.message,
+    })));
 }
