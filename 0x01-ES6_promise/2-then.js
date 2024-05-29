9662d06d802807a584f665864d384bb7c32f8e88
tami-cp0 <findtamilore>
2024-05-29 02:22:21 +0100

    sysntax change

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,11 +1,5 @@
 export default function handleResponseFromAPI(promise) {
-promise.then(() => {
-  return { status: 200, body: 'Success' }
-})
-.catch(() => {
-  throw new Error();
-})
-.finally(() => {
-  console.log('Got a response from the API');
-});
+  promise.then(() => ({ status: 200, body: 'Success' }))
+    .catch(() => { throw new Error(); })
+    .finally(() => console.log('Got a response from the API'));
 }
