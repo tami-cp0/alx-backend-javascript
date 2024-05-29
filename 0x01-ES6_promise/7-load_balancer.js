6c752a4076c9f630387f88157afcd29f146e49b8
tami-cp0 <findtamilore>
2024-05-29 18:55:44 +0100

    task 7 change

--- 0x01-ES6_promise/7-load_balancer.js
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -1,6 +1,6 @@
 export default function loadBalancer(chinaDownload, USDownload) {
   return Promise
-    .any([chinaDownload, USDownload])
+    .race([chinaDownload, USDownload])
     .then((value) => (value))
     .catch((error) => console.log(error));
 }
