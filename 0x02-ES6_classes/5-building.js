a7918b59b94f3dc47e085d9cc296bf0b1242dd5b
tami-cp0 <findtamilore>
2024-05-30 15:48:50 +0100

    task 5 done

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,34 @@
+export default class Building {
+  /**
+   * Constructs a building object with a specified square foot measurement.
+   * @param {number} sqft - The square footage of the building.
+   * @throws {TypeError} If sqft is not a number or if it's infinity.
+   * @throws {Error} if evacuationWarningMessage is not overwritten by the extending class.
+   */
+  constructor(sqft) {
+    if (typeof sqft !== 'number' || !Number.isFinite(sqft)) throw new TypeError('Square foot must be a number');
+    if (this.evacuationWarningMessage === undefined && this.constructor !== Building) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+
+    this._sqft = sqft;
+  }
+
+  /**
+   * Gets the square foot of the building.
+   * @returns {number} The square foot of the building.
+   */
+  get sqft() {
+    return this._sqft;
+  }
+
+  /**
+   * Sets the square foot of the building.
+   * @param {number} sqft - The new square foot of the building.
+   * @throws {TypeError} - If amount is not a finite number.
+   */
+  set sqft(sqft) {
+    if (typeof sqft !== 'number' || !Number.isFinite(sqft)) throw new TypeError('Square foot must be a number');
+    this._sqft = sqft;
+  }
+}
