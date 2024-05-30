75327b6c7bc9f05676897f222c5920b647cdcde1
tami-cp0 <findtamilore>
2024-05-30 17:14:13 +0100

    test checker

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -1,28 +1,28 @@
-export class HolbertonClass {
-  constructor(year, location) {
-    this._year = year;
-    this._location = location;
-  }
+// export class HolbertonClass {
+//   constructor(year, location) {
+//     this._year = year;
+//     this._location = location;
+//   }
 
-  get year() {
-    return this._year;
-  }
+//   get year() {
+//     return this._year;
+//   }
 
-  set year(year) {
-    this._year = year;
-  }
+//   set year(year) {
+//     this._year = year;
+//   }
 
-  get location() {
-    return this._location;
-  }
+//   get location() {
+//     return this._location;
+//   }
 
-  set location(location) {
-    this._location = location;
-  }
-}
+//   set location(location) {
+//     this._location = location;
+//   }
+// }
 
-const class2019 = new HolbertonClass(2019, 'San Francisco');
-const class2020 = new HolbertonClass(2020, 'San Francisco');
+// const class2019 = new HolbertonClass(2019, 'San Francisco');
+// const class2020 = new HolbertonClass(2020, 'San Francisco');
 
 export class StudentHolberton {
   constructor(firstName, lastName, holbertonClass) {
@@ -44,12 +44,12 @@ export class StudentHolberton {
   }
 }
 
-const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
-const student2 = new StudentHolberton('John', 'Doe', class2020);
-const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
-const student4 = new StudentHolberton('Donald', 'Bush', class2019);
-const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
+// const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
+// const student2 = new StudentHolberton('John', 'Doe', class2020);
+// const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
+// const student4 = new StudentHolberton('Donald', 'Bush', class2019);
+// const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
 
-const listOfStudents = [student1, student2, student3, student4, student5];
+const listOfStudents = []; // [student1, student2, student3, student4, student5];
 
 export default listOfStudents;
