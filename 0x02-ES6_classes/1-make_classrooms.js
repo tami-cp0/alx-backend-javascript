383eaee8fda0d65b4c79afc93277f2d512d66ee6
tami-cp0 <findtamilore>
2024-05-30 12:44:36 +0100

    task 1 done

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,16 @@
+import ClassRoom from './0-classroom';
+
+export default function initializeRooms() {
+  const classRooms = [
+    new ClassRoom(19),
+    new ClassRoom(20),
+    new ClassRoom(34),
+  ];
+
+  const objects = [];
+  for (let i = 0; i < 3; i += 1) {
+    objects.push(classRooms[i]);
+  }
+
+  return objects;
+}
