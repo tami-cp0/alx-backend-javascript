a190f8d4001b4e05682eaa85cc0b6f82445a20a2
tami-cp0 <findtamilore>
2024-06-04 19:58:27 +0100

    advanced done

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,10 @@
+const weakMap = new WeakMap();
+
+export function queryAPI(endpoint) {
+  const count = weakMap.get(endpoint) || 0;
+  if (count >= 5) throw new Error('Endpoint load is high');
+  weakMap.set(endpoint, count + 1);
+  return weakMap.get(endpoint);
+}
+
+export { weakMap };
