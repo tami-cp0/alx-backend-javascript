88e7ec18f14b29bbe70be70ecc5df1e23d3bd117
tami-cp0 <findtamilore>
2024-06-04 20:08:13 +0100

    syntax update

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,15 +1,9 @@
 export const weakMap = new WeakMap();
 
 export function queryAPI(endpoint) {
-  let called = weakMap.get(endpoint) || 0;
-
-  called += 1;
-
-  weakMap.set(endpoint, called);
-
-  if (called >= 5) {
-    throw Error('Endpoint load is high');
-  }
-
-  return called;
+  let count = weakMap.get(endpoint) || 0;
+  count += 1;
+  weakMap.set(endpoint, count + 1);
+  if (count >= 5) throw Error('Endpoint load is high');
+  return count;
 }
