57b2611d073632fd5394b8d6bfa92afb6e35d4a4
tami-cp0 <findtamilore>
2024-06-04 18:58:32 +0100

    test

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -3,25 +3,12 @@ export default function cleanSet(set, startString) {
   if (!(set instanceof Set)) throw new TypeError('set must be a Set');
   if (startString === '') return '';
 
-  let string = '';
-
-  // for (const value of set) {
-  //   if (value.startsWith(startString)) {
-  //     strings.push(value.slice(startString.length));
-  //   }
-  // }
-
-  set = Array.from(set);
-
-  set.forEach((value, index) => {
+  const strings = [];
+  for (const value of set) {
     if (value.startsWith(startString)) {
-      if (index === 0) {
-        string += value.slice(startString.length);
-      } else {
-        string += `-${value.slice(startString.length)}`;
-      }
+      strings.push(value.slice(startString.length));
     }
-  });
+  }
 
-  return string;
+  return strings.join('-').toString();
 }
---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,11 @@
+export default function groceriesList() {
+  const gorceries = new Map();
+
+  gorceries.Apples = 10;
+  gorceries.Tomatoes = 10;
+  gorceries.Pasta = 1;
+  gorceries.Rice = 1;
+  gorceries.Banana = 5;
+
+  return gorceries;
+}
