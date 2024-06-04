80cf99a98bf5a639dcb889c27acdd2d42e880683
tami-cp0 <findtamilore>
2024-06-04 19:04:40 +0100

    task 9 done

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,12 +1,11 @@
 export default function groceriesList() {
   const gorceries = new Map();
 
-  gorceries.Apples = 10;
-  gorceries.Tomatoes = 10;
-  gorceries.Pasta = 1;
-  gorceries.Rice = 1;
+  gorceries.set('Apples', 10);
+  gorceries.set('Tomatoes', 10);
+  gorceries.set('Pasta', 1);
+  gorceries.set('Rice', 1);
   gorceries.set('Banana', 5);
-
 
   return gorceries;
 }
