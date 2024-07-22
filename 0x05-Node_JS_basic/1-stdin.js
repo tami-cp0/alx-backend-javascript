62723adafa65f204f2ec8dd28e554718612f3629
tami-cp0 <findtamilore>
2024-07-22 20:00:55 +0100

    process change

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,16 +1,29 @@
-const readline = require('readline');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  if (name) {
+    process.stdout.write(`Your name is: ${name}`);
+  }
 });
 
-rl.question('Welcome to Holberton School, what is your name?\n', (answer) => {
-  console.log(`Your name is: ${answer}`);
-  rl.close();
-  process.exit(0);
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
 
-rl.on('close', () => {
-  console.log('This important software is now closing\n');
-});
+// const readline = require('readline');
+
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
+
+// rl.question('Welcome to Holberton School, what is your name?\n', (answer) => {
+//   console.log(`Your name is: ${answer}`);
+//   rl.close();
+//   process.exit(0);
+// });
+
+// rl.on('close', () => {
+//   console.log('This important software is now closing');
+// });
