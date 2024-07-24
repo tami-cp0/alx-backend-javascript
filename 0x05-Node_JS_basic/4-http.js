c14cbfbe38f6a79a38b527eaed930d18fe814f68
tami-cp0 <findtamilore>
2024-07-24 12:33:13 +0100

    changed syntax

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,9 +1,14 @@
-const app = require('http');
+const http = require('http');
 
-app.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.write('Hello Holberton School!');
-  res.end();
-}).listen(1245);
+const hostname = '127.0.0.1';
+const port = 1245;
+
+const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Holberton School!');
+});
+
+app.listen(port, hostname);
 
 module.exports = app;
