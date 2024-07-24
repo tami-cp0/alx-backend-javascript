e6295cb3ed8aeb359ab06944717babfb094a2751
tami-cp0 <findtamilore>
2024-07-24 22:56:12 +0100

    check

---
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const students = require('./3-read_file_async');
+
+const app = express();
+const port = 1245;
+
+app.get('/', (req, res) => {
+  res.statusCode = 200;
+  res.type('text/plain');
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', (req, res) => {
+  res.statusCode = 200;
+  res.type('text/plain');
+
+  students(process.argv[2]).then((data) => {
+    res.write(`Number of students: ${data.students.length}\n`);
+    res.write(`Number of students in CS: ${data.csStudents.length}. List: ${data.csStudents.join(', ')}\n`);
+    res.write(`Number of students in SWE: ${data.sweStudents.length}. List: ${data.sweStudents.join(', ')}`);
+    res.end();
+  }).catch((err) => {
+    res.status(500).send(err.message);
+  });
+});
+
+app.listen(port);
+
+module.exports = app;
